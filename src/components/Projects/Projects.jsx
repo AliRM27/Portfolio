277ecb5790175f16ab5ae467bce5765e7a40c6d2
Ali Mammadov <amammadov097@gmail.com>
2025-01-12 19:32:16 +0100

    new project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import img1 from '../../assets/project1.png'
 import img2 from '../../assets/project2.png'
+import img3 from '../../assets/project3.png'
 import './Projects.css'
 import Project from '../Project/Project'
 
@@ -13,9 +14,9 @@ const Projects = () => {
         <h1 style={{fontSize:"50px"}}>Projects</h1>
       </div>
       <div className='app__projects-content'>
-        <Project image={img1} name={"GPT-5 Project"} gitLink={"https://github.com/AliRM27/gpt5-website"} demoLink={"https://relaxed-raindrop-413cc0.netlify.app"}/>
-        <Project image={img2} name={"Restaurant"} gitLink={"https://github.com/AliRM27/gericht-restaurant"} demoLink={"https://shiny-jalebi-61f723.netlify.app/"}/>
-
+        <Project image={img1} name={"GPT-5 Landing Page"} gitLink={"https://github.com/AliRM27/gpt5-website"} demoLink={"https://gpt5-landing-page.netlify.app"}/>
+        <Project image={img2} name={"Restaurant"} gitLink={"https://github.com/AliRM27/gericht-restaurant"} demoLink={"https://gericht-restaurant-am.netlify.app"}/>
+        <Project image={img3} name={"Movie Finder"} gitLink={"https://github.com/AliRM27/Movie-finder"} demoLink={"https://movie-finder-am.netlify.app"}/>
       </div>
     </div>
   )
